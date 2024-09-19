c77d6ea364dac20b3aed5da7339f916ffe5101b5
Matheus Gargary <matheusfgargary@gmail.com>
2024-09-19 15:32:26 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#btn-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+
+    $('form').on('submit', function(e) {
+        console.log("submit");
+        e.preventDefault();
+    })
+})
+
